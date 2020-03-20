87ade21bc577f58ba2d148dbdad05e438d2997d0
masatodejima0413 <masatodejima@gmail.com>
2020-03-21 01:49:19 +0900

    create hamburger base

---
+++ src/components/Hamburger/Hamburger.styles.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+import { colors } from '../../styles/variables'
+
+export const StyledHamburger = styled.div`
+  /* background-size: 80px 80px; */
+  position: absolute;
+  top: 30px;
+  right: 30px;
+  fill: ${colors.semiblack};
+  stroke: #ffffff;
+`
--- src/layouts/Background/Background.tsx
+++ src/layouts/Background/Background.tsx
@@ -1,9 +1,15 @@
 import * as React from 'react'
 import { StyledBackground } from './Background.styles'
 import '../../styles/globalstyle.css'
+import Humberger from '../../components/Hamburger/Hamburger'
 
 const Background = ({ children }: any) => {
-  return <StyledBackground>{children}</StyledBackground>
+  return (
+    <StyledBackground>
+      <Humberger />
+      {children}
+    </StyledBackground>
+  )
 }
 
 export default Background
